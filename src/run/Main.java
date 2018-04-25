44faea96ae5b764c2d76d0c22e47a5decf935b9f
PeTrA-Korea <gmlrnjs1994@gmail.com>
2018-04-25 14:19:42 +0900

    start :)

---
+++ src/run/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Project JTuningfork
+ * Musical Tuningfork Engine with JAVA
+ * copyright PeTrA (c) 2018 All rights reserved.
+ */
+
+package run;
+
+public class Main {
+	public static void main(String[] args) {
+		new Main();
+	}
+	
+	/**
+	 * experiment in here
+	 */
+	public Main() {
+		
+	}
+}
