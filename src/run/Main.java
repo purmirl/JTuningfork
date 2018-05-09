7420dccd35d8782e0aa3f2ae3ab6bf6e1146605e
PeTrA-Korea <gmlrnjs1994@gmail.com>
2018-05-09 14:56:10 +0900

    comments is added :)

--- src/run/Main.java
+++ src/run/Main.java
@@ -6,6 +6,11 @@
 
 package run;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.TargetDataLine;
+
 /**
  * Main Class
  * @author PeTrA
@@ -18,8 +23,18 @@ package run;
  * 
  * example of AudioFormat
  *  : AudioFormat audioFormat = new AudioFormat(8000, 8, 1, true, true);
+ *  
+ * example of DataLine.Info
+ *  : DataLine.Info info = new DataLine.Info(TargetDataLine.class, audioFormat)
  * 
- * 
+ * 7 Basic Sound Elements : 
+ * -Loudness
+ * -Pitch
+ * -Contour
+ * -Rhythm
+ * -Tempo
+ * -Spatial Location
+ * -Reverberation
  */
 
 import jtuningfork.module.*;
@@ -33,6 +48,14 @@ public class Main {
 	 * experiment in here
 	 */
 	public Main() {
-
+		AudioFormat audioFormat = new AudioFormat(8000, 8, 1, true, true);
+		DataLine.Info info = new DataLine.Info(TargetDataLine.class, audioFormat);
+		try {
+			JtfSoundReader jtfSoundReader = new JtfSoundReader(audioFormat, info);
+			jtfSoundReader.read();
+		} catch (LineUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
