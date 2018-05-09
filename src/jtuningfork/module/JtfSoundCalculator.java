88c761121ce5fb646c6ef70c4f0adc38f3caf487
PeTrA-Korea <gmlrnjs1994@gmail.com>
2018-05-09 11:19:34 +0900

    byte to double method is added :)

--- src/jtuningfork/module/JtfSoundCalculator.java
+++ src/jtuningfork/module/JtfSoundCalculator.java
@@ -6,11 +6,16 @@
 
 package jtuningfork.module;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+
 /**
  * Calculator of read sound.
  * @author PeTrA
  */
 public class JtfSoundCalculator {
+	
 	public JtfSoundCalculator() {}
 	
 	public void Calculator(byte[] buffer) {
@@ -20,4 +25,28 @@ public class JtfSoundCalculator {
 	private void algorithm(byte[] buffer) {
 		
 	}
+	
+	/**
+	 * trans the byte data to double data
+	 * @param buffer
+	 * @return doubleData
+	 * Reference : https://stackoverflow.com/questions/25666623/my-app-find-frequency-of-audio-input-from-microphone-always-crashs-but-why
+	 */
+	private double[] byteToDouble(byte[] buffer) {
+		ByteBuffer buf = ByteBuffer.wrap(buffer);
+		buf.order(ByteOrder.BIG_ENDIAN);
+		int index = 0;
+		double doubleData[] = null;
+		doubleData = new double[8192];
+		
+		while(buf.remaining() > 2) {
+			short s = buf.getShort();
+			/* audio frame is 8192
+			*/	doubleData[index] = (new Short(s)).doubleValue();
+			++index;
+		}
+		
+		return doubleData;
+	}
+	
 }
